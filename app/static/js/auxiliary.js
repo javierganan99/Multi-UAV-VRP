6bb483f2079bd31a28085fde2d697ced1567318c
atos <fjganan14@gmail.com>
2023-10-14 10:01:14 +0200

    Added string parsing to get the lists in the input fields

--- app/static/js/auxiliary.js
+++ app/static/js/auxiliary.js
@@ -1,21 +1,24 @@
-// Function to check the values of the problem
-function warning(name, warning, min_value = 0) {
-    var element = document.getElementById(name);
-    var elementWarning = document.getElementById(warning);
-    if (element.value.trim() === '' || isNaN(parseInt(element.value.trim())) || parseInt(element.value.trim()) <= min_value) {
-        elementWarning.classList.remove('hidden');
-        event.preventDefault();
-        return true;
-    } else {
-        elementWarning.classList.add('hidden');
+function stringNumbersToList(str, numbers_to_load) {
+    const parts = str.split(/[,\s]+/).filter(part => part !== '');
+    if (parts.length === 1 && numbers_to_load == 1) {
+      const singleNumber = parseFloat(parts[0]);
+      return isNaN(singleNumber) ? "Invalid input: Not a valid number" : singleNumber;
+    } else if (parts.length > 1) {
+      const numbers = parts.map(part => parseFloat(part));
+      const allNumbers = numbers.every(num => !isNaN(num));
+      if (allNumbers && numbers.length == numbers_to_load) {
+        return numbers;
+      } else {
         return false;
+      }
+    } else {
+      return false;
     }
 }
 
 // Function to reset the problem information
 function reset(event) {
     event.preventDefault();
-    var check_list = ["n_vehicles", "max_flight_time", "capacity", "velocity"];
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/reset", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
