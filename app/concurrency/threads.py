36f9355bf589f8a50299d0d73c77a373d7141f96
atos <fjganan14@gmail.com>
2023-10-14 09:42:21 +0200

    Deleted files, not finally needed

--- app/concurrency/threads.py
+++
@@ -1,9 +0,0 @@
-def wait_threads(threads):
-    """
-    Wait for all threads in the list to complete execution.
-
-    Parameters:
-    threads (list): List of `Thread` objects to wait for.
-    """
-    for t in threads:
-        t.join()
