c9e15d33442fe78cab9c783170e4565a98e2b26f
atos <fjganan14@gmail.com>
2023-10-14 09:56:10 +0200

    Adapt problem to multi-depot and heterogeneous swarm

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, current_app, request, jsonify
 from flask_babel import gettext
-from app.utils.load_parameters import load_solver_configuration
-from app.utils.auxiliary import generate_solution
+from app.utils.load_parameters import load_solver_configuration, load_problem_definiton
+from app.utils.auxiliary import generate_solution, string2value
 from app.utils.or_tools_method import find_routes
 from app.utils.problem_definiton import (
     generate_distance_matrix,
     generate_flight_distance_matrix,
 )
-from app.utils.simulation import SimulationPath
+
 
 # Define a Flask blueprint for handling routes-related routes
 routes_blueprint = Blueprint("routes_blueprint", __name__)
@@ -22,21 +22,34 @@ def generate_route():
     if request.method == "POST":
         if not current_app.solver_data:
             current_app.solver_data = load_solver_configuration()
-        current_app.problem_data["n_vehicles"] = int(request.form["n_vehicles"])
-        current_app.problem_data["max_flight_time"] = int(
-            request.form["max_flight_time"]
-        )
-        current_app.problem_data["capacity"] = int(request.form["capacity"])
-        current_app.problem_data["velocity"] = int(request.form["velocity"])
+        if not current_app.problem_data:  # Load defaults
+            current_app.problem_data = load_problem_definiton()
+        check_list = [
+            "n_vehicles",
+            "max_flight_time",
+            "velocity",
+            "start_nodes",
+            "end_nodes",
+        ]
+        for name in check_list:
+            value = string2value(request.form[name])
+            if not value:
+                return jsonify(
+                    success=False,
+                    message=gettext("Error parsing the values of the problem data"),
+                )
+            current_app.problem_data[name] = value
         # Distance matrix creation
         if "distance_matrix" not in current_app.problem_data.keys():
             if current_app.problem_data["travel_mode"] == "flight":
                 current_app.problem_data[
                     "distance_matrix"
-                ] = generate_flight_distance_matrix(current_app.coordinates_list)
+                ] = generate_flight_distance_matrix(
+                    current_app.problem_data["addresses"]
+                )
             else:
                 current_app.problem_data["distance_matrix"] = generate_distance_matrix(
-                    current_app.coordinates_list,
+                    current_app.problem_data["addresses"],
                     current_app.config["API_KEY"],
                     current_app.config["GEOCODE_API_URL"],
                     current_app.config["DISTANCE_MATRIX_API_URL"],
@@ -47,19 +60,11 @@ def generate_route():
             current_app.problem_data, current_app.solver_data
         )
         current_app.routes = generate_solution(
-            data, manager, routing, solution, current_app.coordinates_list
+            data, manager, routing, solution, current_app.problem_data["addresses"]
         )
-        # TODO: Eliminate, integrate on simulation route
-        # simulation_path = SimulationPath(
-        #     current_app.routes["routes"],
-        #     current_app.problem_data["distance_matrix"],
-        #     velocity=10,
-        #     timestep=0.1,
-        # )
-        # for coords in simulation_path:
-        #     pass
         return jsonify(
             success=True,
             message=gettext("Routes created successfully"),
             routes=current_app.routes,
+            problem_data=current_app.problem_data,
         )
