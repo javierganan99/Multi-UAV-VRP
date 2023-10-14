b48d72592a5d3e65619285e34324fa28af58e9e3
atos <fjganan14@gmail.com>
2023-10-14 10:10:57 +0200

    Added new variables to support multi-depot and heterogeneity

--- app/static/js/variables.js
+++ app/static/js/variables.js
@@ -1,6 +1,9 @@
 var map; // Map
-var markers = []; // Markers
+var markers = []; // Markers of the locations
+var pose_markers = {}; // Markers of the pose of the drones 
 var infoWindow; // Info of the markers
 var paths = []; // Routes
-var vehicleImages = {}; // A map to store img elements by vehicle id
-var vehicleSockets = {}; // A map to store socket connections by vehicle id
+var n_vehicles = 0; // Number of current vehicles
+const drone_icon = "images/drone.ico" // Icon for the drone pose 
+var eventSourcePose; // The surce to stream the UAVs pose
+const check_list = ["n_vehicles", "max_flight_time", "velocity", "start_nodes", "end_nodes"]; // Input fields to check
