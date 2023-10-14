9392fd2ddc03a797f049442b70093fd1b93b7fc7
atos <fjganan14@gmail.com>
2023-10-14 10:02:44 +0200

    Changes in addMarker functions. Added addPoseMarker function to display the drone icon and drawProbelmData to display the location markers

--- app/static/js/drawing.js
+++ app/static/js/drawing.js
@@ -1,5 +1,6 @@
 // Adds a marker to the map
 function addMarker(coordinates, index, color = "red") {
+    console.log(markers.length);
     let svgMarker = { // Style of the marker
         path: "M-1.547 12l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM0 0q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
         fillColor: color,
@@ -9,9 +10,6 @@ function addMarker(coordinates, index, color = "red") {
         scale: 2,
         anchor: new google.maps.Point(0, 20),
     };
-    if (index === 0 && markers.length !== 0) {
-        markers[index].setMap(null);
-    }
     if (index < markers.length && index != 0) {
         console.log("Marker already added");
         return 0;
@@ -23,12 +21,7 @@ function addMarker(coordinates, index, color = "red") {
         label: index.toString(),
         icon: svgMarker
     });
-    if (index === 0) {
-        markers.splice(index, 1, marker);
-    } else {
-        markers.splice(index, 0, marker);
-    }
-
+    markers.push(marker); // Add marker
     // The content of each marker
     marker.addListener("mouseover", () => {
         infoWindow.close();
@@ -50,7 +43,7 @@ function addMarker(coordinates, index, color = "red") {
 }
 
 // Deletes a marker from the map
-function deleteMarker(index, coordinates = null) {
+function deleteMarker(index) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/delete-address", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -60,11 +53,11 @@ function deleteMarker(index, coordinates = null) {
                 var response = JSON.parse(xhr.responseText);
                 if (response.success) {
                     console.log("Address deleted successfully");
-                    markers[index].setMap(null);
-                    if (index !== 0) {
-                        markers = [markers[0]];
-                        getMap(response.coordinates); // To reorder the indexes
+                    for (let i = 0; i < markers.length; i++) {
+                        markers[i].setMap(null);
                     }
+                    markers = [];
+                    drawProblemData(response.problem_data); // To reorder the indexes
                 } else {
                     // Failed to convert address
                     console.error("Failed to convert address:", response.message);
@@ -77,6 +70,13 @@ function deleteMarker(index, coordinates = null) {
     };
     // Send the index as the request body
     xhr.send("index=" + encodeURIComponent(index));
+}
+
+function deleteAllMarkers() {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
+    markers = [];
 }
 
 // Function to draw the routes
@@ -98,4 +98,34 @@ function drawRoutes(routes_info) {
         paths[v_number].setMap(map);
     });
 
+}
+
+// Adds a drone pose marker to the map
+function addPoseMarker(coordinates, icon) {
+    return new google.maps.Marker({
+        position: coordinates,
+        map,
+        icon: icon
+    });
+}
+
+// Display markers
+function drawProblemData(problem_data) {
+    for (let i = 0; i < problem_data["addresses"].length; i++) {
+        if (problem_data["start_nodes"].includes(i) && problem_data["end_nodes"].includes(i)) {
+            // Both Departure and Arrival Depot
+            addMarker(problem_data["addresses"][i], i, "black");
+        } else if (problem_data["start_nodes"].includes(i)) {
+            // Departure depot
+            addMarker(problem_data["addresses"][i], i, "blue");
+        }
+        else if (problem_data["end_nodes"].includes(i)) {
+            // Arrival depot
+            addMarker(problem_data["addresses"][i], i, "purple");
+        }
+        else{
+            // To visit node
+            addMarker(problem_data["addresses"][i], i, "red");
+        }
+    }
 }
