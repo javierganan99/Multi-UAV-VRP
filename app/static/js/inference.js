5003ad4076453d5ff2a5515f17f98959493cee0d
atos <fjganan14@gmail.com>
2023-10-14 10:04:05 +0200

    Added functions to check the input boxes content for heterogeneity and multi-depot

--- app/static/js/inference.js
+++ app/static/js/inference.js
@@ -1,13 +1,48 @@
+// Function to check if the depots are in the markers
+function check_depots_in_markers(depots) {
+    for (var depot of depots) {
+        if (depot > (markers.length - 1) || depot < 0) {
+            return false
+        }
+    }
+    return true
+}
+
+
+// Function to check the values of the problem
+// TODO: CHECK MIN VALUE
+function no_warning(name, warning, min_value = 0) {
+    if (name == "n_vehicles") {
+         var numbers_to_load = 1;
+    } else {
+        numbers_to_load = n_vehicles;
+    }
+    var element = document.getElementById(name);
+    var elementWarning = document.getElementById(warning);
+    var new_element = stringNumbersToList(element.value, numbers_to_load);
+    if ((!new_element) || (check_list.slice(3).includes(name) && !check_depots_in_markers(new_element))) {
+        elementWarning.classList.remove('hidden');
+        return false;
+    } else {
+        elementWarning.classList.add('hidden');
+        if (name == "n_vehicles") {
+            n_vehicles = new_element;
+        }
+        return new_element;
+    }
+}
+
 // Function to generate the routes
 function generateRoutes(event) {
     event.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/create-routes", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    var check_list = ["n_vehicles", "max_flight_time", "capacity", "velocity"]
     // Check for any invalid value
+    var returned_param;
     for (let i = 0; i < check_list.length; i++) {
-        if (warning(check_list[i], check_list[i] + "-warning")) {
+        returned_param = no_warning(check_list[i], check_list[i] + "-warning")
+        if (!returned_param) {
             return false
         }
     }
@@ -16,9 +51,12 @@ function generateRoutes(event) {
             if (xhr.status === 200) {
                 var response = JSON.parse(xhr.responseText);
                 if (response.success) {
-                    let routes = response.routes;
-                    drawRoutes(routes);
-                    console.log("Route generation successful");
+                    // Delete the current problem data
+                    deleteAllMarkers();
+                    // Draw problem data
+                    drawProblemData(response.problem_data);
+                    // Draw the routes
+                    drawRoutes(response.routes);
                 } else {
                     console.error("Route generation failed:", response.message);
                 }
