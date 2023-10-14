f2255bf5eb747b182b014f8e40c670a0df23c754
atos <fjganan14@gmail.com>
2023-10-14 09:57:05 +0200

    Minor changes, same functionality

--- app/routes/save.py
+++ app/routes/save.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, current_app, request, jsonify
 from flask_babel import gettext
-from app.utils.auxiliary import save_yaml, ensure_exist
+from app.utils.auxiliary import save_yaml, ensure_folder_exist
 
 # Define a Flask blueprint for handling saving-related routes
 save_blueprint = Blueprint("save_blueprint", __name__)
@@ -15,8 +15,8 @@ def save_nodes(file="output/nodes2visit.yaml"):
     file (str, optional): The path of the file to save to. Default is "output/nodes2visit.yaml".
     """
     if request.method == "POST":
-        ensure_exist("/".join(file.split("/")[:-1]))
-        data = {"addresses": current_app.coordinates_list}
+        ensure_folder_exist("/".join(file.split("/")[:-1]))
+        data = {"addresses": current_app.problem_data["addresses"]}
         save_yaml(file, data)
         return jsonify(success=True, message=gettext("Nodes saved"))
 
@@ -30,7 +30,7 @@ def save_routes(file="output/routes.yaml"):
     file (str, optional): The path of the file to save to. Default is "output/routes.yaml".
     """
     if request.method == "POST":
-        ensure_exist("/".join(file.split("/")[:-1]))
+        ensure_folder_exist("/".join(file.split("/")[:-1]))
         data = {"routes": current_app.routes}
         save_yaml(file, data)
         return jsonify(success=True, message=gettext("Routes saved"))
