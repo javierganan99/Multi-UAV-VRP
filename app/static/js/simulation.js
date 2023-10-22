f6849e3613ad952971e792236f7e538b388fa329
atos <fjganan14@gmail.com>
2023-10-22 13:26:13 +0200

    Imports and exports added, and minor style changes

--- app/static/js/simulation.js
+++ app/static/js/simulation.js
@@ -1,35 +1,39 @@
+import { globalElements } from './globals.js'
+import { updatePoseMarkers } from './monitor.js'
+import { addPoseMarker } from './drawing.js'
+export { startSimulation, stopSimulation }
 // To start the simulation
 function startSimulation () {
   // Toggle the text of the Simulation button
-  var sim_button = document.getElementById('simulation')
-  sim_button.textContent = 'Stop Simulation'
+  const simButton = document.getElementById('simulation')
+  simButton.textContent = 'Stop Simulation'
   // Toggle function associated
-  sim_button.onclick = stopSimulation
-  // Update pose_markers of n_vehicles
-  updatePoseMarkers(n_vehicles)
-  eventSourcePose = new EventSource('/simulation')
-  eventSourcePose.onmessage = function (event) {
-    if (event.data == 'None') {
+  simButton.onclick = stopSimulation
+  // Update poseMarkers of nVehicles
+  updatePoseMarkers(globalElements.nVehicles)
+  globalElements.eventSourcePose = new EventSource('/simulation')
+  globalElements.eventSourcePose.onmessage = function (event) {
+    if (event.data === 'None') {
       stopSimulation()
       return
     }
     const data = JSON.parse(event.data)
     for (let i = 0; i < data.length; i++) {
-      ml = pose_markers[i].length
+      const ml = globalElements.poseMarkers[i].length
       if (ml >= 1) {
-        pose_markers[i].push(
+        globalElements.poseMarkers[i].push(
           addPoseMarker(
-            { lat: data[i]['lat'], lng: data[i]['lon'] },
-            drone_icon
+            { lat: data[i].lat, lng: data[i].lon },
+            globalElements.droneIcon
           )
         ) // Add new position
-        pose_markers[i][ml - 1].setMap(null) // Oclude last marker
-        pose_markers[i].slice(ml - 1, 1) // Eliminate marker from list
+        globalElements.poseMarkers[i][ml - 1].setMap(null) // Oclude last marker
+        globalElements.poseMarkers[i].slice(ml - 1, 1) // Eliminate marker from list
       } else {
-        pose_markers[i].push(
+        globalElements.poseMarkers[i].push(
           addPoseMarker(
-            { lat: data[i]['lat'], lng: data[i]['lon'] },
-            drone_icon
+            { lat: data[i].lat, lng: data[i].lon },
+            globalElements.droneIcon
           )
         )
       }
@@ -40,33 +44,32 @@ function startSimulation () {
 // To stop the simulation
 function stopSimulation () {
   // Toggle the text of the Simulation button
-  var sim_button = document.getElementById('simulation')
-  sim_button.textContent = 'Simulation'
+  const simButton = document.getElementById('simulation')
+  simButton.textContent = 'Simulation'
   // Toggle function associated
-  sim_button.onclick = startSimulation
-  if (eventSourcePose) {
-    eventSourcePose.close()
+  simButton.onclick = startSimulation
+  if (globalElements.eventSourcePose) {
+    globalElements.eventSourcePose.close()
   }
   // Delete the UAVs' markers
-  pml = Object.keys(pose_markers).length
-  for (let i = 0; i < Object.keys(pose_markers).length; i++) {
-    for (let j = 0; j < pose_markers[i].length; j++) {
-      pose_markers[i][j].setMap(null) // Oclude marker
+  for (let i = 0; i < Object.keys(globalElements.poseMarkers).length; i++) {
+    for (let j = 0; j < globalElements.poseMarkers[i].length; j++) {
+      globalElements.poseMarkers[i][j].setMap(null) // Oclude marker
     }
   }
-  pose_markers = {}
-  var xhr = new XMLHttpRequest()
+  globalElements.poseMarkers = {}
+  const xhr = new XMLHttpRequest()
   xhr.open('POST', '/stop-simulation', true)
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE) {
       if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText)
+        const response = JSON.parse(xhr.responseText)
         if (response.success) {
           console.log('Simulation stop signal sended!')
         } else {
           console.error(
-            'Error stopping the simulation: ' + file,
+            'Error stopping the simulation: ' +
             response.message
           )
         }
