1229afe96fdbed60d001ab20b578ca74977ad84d
atos <fjganan14@gmail.com>
2023-10-22 13:15:50 +0200

    Removed load_solver_confguration route, solver configuration configured in the server side

--- app/routes/loaders.py
+++ app/routes/loaders.py
@@ -1,16 +1,17 @@
 from flask import Blueprint, current_app, request, jsonify
-from utils.problem_definiton import detect_address_format
-from utils.load_parameters import load_problem_definiton, load_solver_configuration
 from flask_babel import gettext
+from utils.problem_definiton import detect_address_format
+from utils.load_parameters import load_problem_definiton
 
 # Define a Flask blueprint for handling loading-related routes
 loaders_blueprint = Blueprint("loaders_blueprint", __name__)
 
 
-@loaders_blueprint.route("/load-problem", methods=["POST"])
+@loaders_blueprint.route("/load-problem", methods=["GET"])
 def load_yaml_problem():
     """
-    Load problem definition data from a YAML file, convert the addresses to coordinates, and store them in the application context.
+    Load problem definition data from a YAML file, convert the addresses to coordinates,
+        and store them in the application context.
 
     Args:
         None
@@ -18,18 +19,18 @@ def load_yaml_problem():
     Returns:
         None"""
     coord_inds = []
-    if request.method == "POST":
+    if request.method == "GET":
         current_app.problem_data = load_problem_definiton()
         # Addresses to the proper format
         for i, address in enumerate(current_app.problem_data["addresses"]):
-            format = detect_address_format(address)
-            if format == "str-coordinates":
+            c_format = detect_address_format(address)
+            if c_format == "str-coordinates":
                 coordinates = address.replace(" ", "").split(",")
-            elif format == "name":
+            elif c_format == "name":
                 coordinates = current_app.converter.address2coords(address)
             else:
                 coordinates = address
-            if coordinates == None:
+            if coordinates is None:
                 return jsonify(
                     success=False,
                     message=gettext("Failed to convert address"),
@@ -47,24 +48,4 @@ def load_yaml_problem():
             success=True,
             message=gettext("Data loaded properly"),
             problem_data=current_app.problem_data,
-        )
-
-
-@loaders_blueprint.route("/load-solver", methods=["POST"])
-def load_yaml_solver():
-    """
-    Load solver configuration data from a YAML file and store them in the application context.
-
-    Args:
-        None
-
-    Returns:
-        None"""
-    if request.method == "POST":
-        file = request.form["file"]
-        current_app.solver_data = load_solver_configuration(file)
-        return jsonify(
-            success=True,
-            message=gettext("Data loaded properly"),
-            solver_data=current_app.solver_data,
         )
