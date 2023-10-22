3f8221fb525dd6d757f4af601927c8150ca0947a
atos <fjganan14@gmail.com>
2023-10-22 13:16:44 +0200

    Corrected the way the default problem definition is overwritten by GUI

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, current_app, request, jsonify
 from flask_babel import gettext
 from utils.load_parameters import load_solver_configuration, load_problem_definiton
-from utils.auxiliary import adapt_solution, string2value
+from utils.auxiliary import adapt_solution, string2list
 from utils.or_tools_method import find_routes
 from utils.problem_definiton import (
     generate_distance_matrix,
@@ -16,17 +16,19 @@ routes_blueprint = Blueprint("routes_blueprint", __name__)
 @routes_blueprint.route("/create-routes", methods=["POST"])
 def generate_route():
     """
-    Generate a route based on the provided form data and the problem and solver data stored in the application context.
-    This includes generating a distance matrix if it doesn't exist, finding routes using OR-Tools, and generating a solution.
+    Generate a route based on the provided form data and the
+    problem and solver data stored in the application context.
+    This includes generating a distance matrix if it doesn't exist,
+    finding routes using OR-Tools, and generating a solution.
 
     Returns:
-        JSON object: A JSON object containing the success status, a message, the generated routes, and the problem data.
+        JSON object: A JSON object containing the success status,
+            a message, the generated routes, and the problem data.
     """
     if request.method == "POST":
         if not current_app.solver_data:
             current_app.solver_data = load_solver_configuration()
-        if not current_app.problem_data:  # Load defaults
-            current_app.problem_data = load_problem_definiton()
+        default_problem_data = load_problem_definiton()
         check_list = [
             "n_vehicles",
             "max_flight_time",
@@ -34,15 +36,19 @@ def generate_route():
             "start_nodes",
             "end_nodes",
         ]
+        new_problem_data = {}
         for name in check_list:
-            value = string2value(request.form[name])
+            value = string2list(request.form[name])
             if not value:
                 return jsonify(
                     success=False,
                     message=gettext("Error parsing the values of the problem data"),
                 )
-            current_app.problem_data[name] = value
-        current_app.problem_data["travel_mode"] = request.form["travel_mode"]
+            new_problem_data[name] = value if name != "n_vehicles" else value[0]
+        new_problem_data["addresses"] = current_app.problem_data["addresses"]
+        new_problem_data["travel_mode"] = request.form["travel_mode"]
+        # Overwrite default problem data
+        current_app.problem_data = {**default_problem_data, **new_problem_data}
         # Distance matrix creation
         if "distance_matrix" not in current_app.problem_data.keys():
             if current_app.problem_data["travel_mode"] == "flight":
