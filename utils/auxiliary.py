0d84e00bd1efe433361b5788dfc0cdf2dd7e6b18
atos <fjganan14@gmail.com>
2023-10-22 13:29:43 +0200

    Minor style changes

--- utils/auxiliary.py
+++ utils/auxiliary.py
@@ -1,11 +1,11 @@
-import yaml
 import os
 import time
-import cv2
 import base64
-import numpy as np
 import re
 from typing import Dict
+import yaml
+import cv2
+import numpy as np
 
 
 # Decorators
@@ -30,23 +30,24 @@ def measure_execution_time(func):
     return wrapper
 
 
-def string2value(input_str):
+def string2list(input_str):
     """
-    Converts a string to a value. If the string contains only one value, the function returns that value as an integer.
-    If the string contains multiple values separated by commas, the function returns a list of those values as integers.
+    Converts a string to a list. If the string contains only one value,
+    the function returns that value as a list containing that integer.
+    If the string contains multiple values separated by commas,
+    the function returns a list of those values as integers.
 
     Args:
         input_str (str): The input string to be converted.
 
     Returns:
-        int or list(int): The converted value(s) as integers. If the string contains only one value, it is returned as an integer.
+        int or list(int): The converted value(s) as integers.
+        If the string contains only one value, it is returned as an integer.
         If the string contains multiple values, they are returned as a list of integers.
     """
     parts = input_str.split(",")
     parts = [p.strip() for p in parts if p.strip() != ""]
-    if len(parts) == 1:
-        return int(parts[0])
-    elif len(parts) > 1:
+    if len(parts) >= 1:
         return [int(i) for i in parts]
     else:
         return False
@@ -115,7 +116,7 @@ def load_yaml(file: str = "data.yaml") -> Dict:
 
     Returns:
         Dict: Dictionary containing the contents of the YAML file."""
-    assert os.path.exists(file), "File not found in path {}".format(file)
+    assert os.path.exists(file), f"File not found in path {file}"
     assert isinstance(file, str) and file.endswith(
         ".yaml"
     ), "Not the proper format, must be a yaml file string variable"
@@ -150,7 +151,8 @@ def ensure_folder_exist(path):
     """
     Ensure that a folder exists at the specified path.
 
-    This function takes a path as input and creates any necessary folders in the path so that a folder exists at the final path.
+    This function takes a path as input and creates any necessary folders in the
+    path so that a folder exists at the final path.
     If the folder(s) already exist, the function does not perform any action.
 
     Args:
@@ -182,7 +184,8 @@ def generate_n_colors(n):
     """
     Generates a list of n colors using the HSV color space.
 
-    This function takes an integer n as input and generates a list of n colors in the form of hexadecimal RGB values.
+    This function takes an integer n as input and generates
+    a list of n colors in the form of hexadecimal RGB values.
     The colors are evenly distributed in hue with maximum saturation and value.
 
     Args:
@@ -225,7 +228,7 @@ def adapt_solution(data, manager, routing, solution, coordinates_list):
     for vehicle_id in range(data["n_vehicles"]):
         routes_dict["routes"][vehicle_id] = {}
         index = routing.Start(vehicle_id)
-        plan_output = "Route for vehicle {}:\n".format(vehicle_id)
+        plan_output = f"Route for vehicle {vehicle_id}:\n"
         route_dist = 0
         nodes = []
         coordinates = []
@@ -233,7 +236,7 @@ def adapt_solution(data, manager, routing, solution, coordinates_list):
             node = manager.IndexToNode(index)
             nodes.append(node)
             coordinates.append(coordinates_list[node])
-            plan_output += " {} -> ".format(node)
+            plan_output += f" {node} -> "
             previous_index = index
             index = solution.Value(routing.NextVar(index))
             route_dist += routing.GetArcCostForVehicle(
@@ -246,10 +249,8 @@ def adapt_solution(data, manager, routing, solution, coordinates_list):
         route_time = int(route_dist / velocity)
         route_mins = route_time // 60
         route_secs = route_time % 60
-        plan_output += "{}\n".format(manager.IndexToNode(index))
-        plan_output += "Time to complete the route: {}min {}secs\n".format(
-            route_mins, route_secs
-        )
+        plan_output += f"{manager.IndexToNode(index)}\n"
+        plan_output += f"Time to complete the route: {route_mins}min {route_secs}secs\n"
         print(plan_output)
         max_route_time = max(route_time, max_route_time)
         routes.append(nodes)
@@ -263,8 +264,6 @@ def adapt_solution(data, manager, routing, solution, coordinates_list):
     max_route_secs = max_route_time % 60
     routes_dict["total_time"] = max_route_time
     print(
-        "Time to complete the longest route: {}min {}secs\n".format(
-            max_route_mins, max_route_secs
-        )
+        f"Time to complete the longest route: {max_route_mins}min {max_route_secs}secs\n"
     )
     return routes_dict
